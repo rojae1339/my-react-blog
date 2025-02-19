49d77875473e98f529fcd7521dfb10963a7571fb
rojae1339 <wjdwogh1224@gmail.com>
2025-02-19 16:10:32 +0100

    fix

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -10,10 +10,10 @@ const Profile = () => {
         <div
             className={`
                 flex flex-col min-h-screen max-h-fit items-start gap-3 bg-[#fcf5ed] bg-cover
-                pt-[60px] pb-6 px-6 w-[280px] min-w-[280px]   /* 기본 크기 (모바일) */
-                sm:w-[300px] sm:min-w-[300px] sm:pt-[70px] sm:px-8 /* 작은 태블릿 */
-                md:w-[320px] md:min-w-[320px] md:pt-[80px] md:px-10 /* 태블릿 */
-                lg:w-[364px] lg:min-w-[364px] lg:pt-[90px] lg:px-14 /* 데스크탑 */
+                pt-[60px] pb-6 px-6 w-[240px] min-w-[240px]   /* 기본 크기 (모바일) */
+                sm:w-[260px] sm:min-w-[260px] sm:pt-[70px] sm:px-6 /* 작은 태블릿 */
+                md:w-[300px] md:min-w-[300px] md:pt-[80px] md:px-6 /* 태블릿 */
+                lg:w-[350px] lg:min-w-[350px] lg:pt-[90px] lg:px-10 /* 데스크탑 */
             `}
         >
             <ProfilePicture />
--- src/components/profile/ProfileLink.jsx
+++ src/components/profile/ProfileLink.jsx
@@ -4,7 +4,7 @@ const ProfileLink = () => {
 
 
     return (
-        <div className={"flex flex-row pt-[320px] pr-14 w-full text-xl items-center justify-center gap-5"}>
+        <div className={"flex flex-row pt-[320px] pr-0 sm:pr-1 md:pr-4 lg:pr-8 w-full text-xl items-center justify-center gap-5"}>
             {IconLink.map((item, index) => {
                 return (
                     <a key={`link_${index}`} href={item.link} target={"_blank"}>
